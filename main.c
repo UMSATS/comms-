3ad2c18caa55708bcc605d4012973b48b1f9bcaa
Hungry Skeleton <Tranryan13@gmail.com>
2019-01-09 15:35:11 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
+#define MASK(BIT)   (1 << BIT)
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+#define spiDeselect()   (NSEL_PORT |= MASK(NSEL_BIT) // Sets NSEL high through the use of a mask
+int main()
+{
+
 }
 
 void init()
 {
-    
-    
-    
+    spiDeselect(); // Disable communication to the radio
+    // Initializing CSN, SDN, and IRQ
+    CSN_PORT
+    spi_init(); 
+   
 }
 
-//COMMENT
+
+/*-------------------------------
+LIST OF VARIABLES TO ASSIGN
+-----------------------------
+NSEL_BIT
+NSEL_PORT
+
+---------------------------------*/
