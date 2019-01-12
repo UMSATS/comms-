832ad762dee20001d446a053fd792ffb0cc12d49
Hungry Skeleton <Tranryan13@gmail.com>
2019-01-12 14:27:35 -0600

    Update radio_functions.h

--- radio_functions.h
+++ radio_functions.h
@@ -1,1 +1,14 @@
-//Radio Functions
+//Radio Functions Header File
+
+#define NSEL_DDR
+#define NSEL_PORT
+#define NSEL_BIT
+
+#define SDN_DDR
+#define SDN_PORT
+#define SDN_BIT
+
+#define IRQ_DDR
+#define IRQ_PORT
+#define IRQ_BIT
+#define IRQ_PUE
